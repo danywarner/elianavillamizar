79b2e9cd4448391b5c80432571e8ca2050b0829a
Daniel Warner <danywarner@gmail.com>
2016-02-02 19:34:31 -0500

    navigation working: changing label from page to page

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 (function(){
+
+var loadPage = function(number) {
+  var label = document.getElementById("pageText")
+  label.innerHTML = "Pagina ".concat(number)
+};
   
 var counter = 0, // to keep track of current slide
     $items = document.querySelectorAll('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
@@ -27,6 +32,46 @@ document.querySelector('.next').addEventListener('click', function() {
 document.querySelector('.prev').addEventListener('click', function() {
      counter--;
      showCurrent();
+  }, false);
+
+document.querySelector('#button1').addEventListener('click', function() {
+     loadPage('1')
+  }, false);
+
+document.querySelector('#button2').addEventListener('click', function() {
+     loadPage('2')
+  }, false);
+
+document.querySelector('#button3').addEventListener('click', function() {
+     loadPage('3')
+  }, false);
+
+document.querySelector('#button4').addEventListener('click', function() {
+     loadPage('4')
+  }, false);
+
+document.querySelector('#button5').addEventListener('click', function() {
+     loadPage('5')
+  }, false);
+
+document.querySelector('#button6').addEventListener('click', function() {
+     loadPage('6')
+  }, false);
+
+document.querySelector('#button7').addEventListener('click', function() {
+     loadPage('7')
+  }, false);
+
+document.querySelector('#button8').addEventListener('click', function() {
+     loadPage('8')
+  }, false);
+
+document.querySelector('#button9').addEventListener('click', function() {
+     loadPage('9')
+  }, false);
+
+document.querySelector('#button10').addEventListener('click', function() {
+     loadPage('10')
   }, false);
   
 })();  
