e4f9b6704d2bc4154f736d039553b26e6f2b9bf6
Daniel Warner <danywarner@gmail.com>
2016-02-21 16:32:46 -0500

    code cleanup 1

--- js/script.js
+++ js/script.js
@@ -49,10 +49,6 @@ var setPage = function(pageNumber) {
 };
 
 var loadPage = function() {
-  var label = document.getElementById("pageText");
-  label.innerHTML = "Pagina ".concat(currentPage);
-  var label1 = document.getElementById("groupText");
-  label1.innerHTML = "Grupo ".concat(currentGroup);
   showData();
 };
 
@@ -222,6 +218,8 @@ var showData = function() {
 
   loadTeam();
   loadInputsAndOutputs();
+  
+
 
 };
 
@@ -302,7 +300,7 @@ var loadTeam = function() {
 
 };
 
-function loadTable(e){
+function loadTable(e){ 
      if (!e)
         e = window.event;
     var sender = e.srcElement || e.target;
@@ -399,23 +397,23 @@ function loadDDTable() {
   ddTable.innerHTML = descDetalladaText;
 }
   
-var counter = 0, // to keep track of current slide
-    $items = document.querySelectorAll('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
-    numItems = $items.length; // total number of slides
+//var counter = 0, // to keep track of current slide
+    //$items = document.querySelectorAll('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
+    //numItems = $items.length; // total number of slides
 
 // this function is what cycles the slides, showing the next or previous slide and hiding all the others
-var showCurrent = function(){
-  var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
+// var showCurrent = function(){
+//   var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
   
-  // remove .show from whichever element currently has it 
-  // http://stackoverflow.com/a/16053538/2006057
-  [].forEach.call( $items, function(el){
-    el.classList.remove('show');
-  });
+//   // remove .show from whichever element currently has it 
+//   // http://stackoverflow.com/a/16053538/2006057
+//   [].forEach.call( $items, function(el){
+//     el.classList.remove('show');
+//   });
   
-  // add .show to the one item that's supposed to have it
-  $items[itemToShow].classList.add('show');    
-};
+//   // add .show to the one item that's supposed to have it
+//   $items[itemToShow].classList.add('show');    
+// };
 
 window.onload = function() {
   currentGroup = urlParam("groupName");
@@ -425,7 +423,7 @@ window.onload = function() {
 };
 
 
-document.querySelector('#button1').addEventListener('click', function(e) {
+document.querySelector('#button1').addEventListener('click', function() {
      setPage('1');
      loadPage();
   }, false);
